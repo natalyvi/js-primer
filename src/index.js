8ad08e96ce2b94992c6a11aabbb70f750f28a75b
Nataly Villalba <alexandra191192@hotmail.com>
2023-03-06 20:02:22 -0500

    primer assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-export function calculateTotal (items, tax) {
-  // TODO
+export function calculateTotal(items, tax) {
+    let total = 0
+    items.forEach(item => {
+        total = total + item.price
+        if (item.taxable === true) {
+            let taxPrice = item.price * Math.abs(tax)
+            total = total + taxPrice
+        }
+    })
+    return total
 }
